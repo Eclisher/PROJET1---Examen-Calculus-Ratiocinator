3326428e8842fe084e4d86c7dea9c330382058fc
Eclisher <hei.harizo@gmail.com>
2024-06-07 13:31:42 +0300

    feat: Initial project

---
+++ src/main/java/projet1/calculus/Projet1ExamenCalculusRatiocinatorApplication.java
@@ -0,0 +1,13 @@
+package projet1.calculus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet1ExamenCalculusRatiocinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet1ExamenCalculusRatiocinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projet1/calculus/Projet1ExamenCalculusRatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package projet1.calculus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet1ExamenCalculusRatiocinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
